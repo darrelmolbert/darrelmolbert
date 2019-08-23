a9c7d3ec0202f49db25b56ea6aa38ff7f6086ee8
darrelmolbert <52434033+darrelmolbert@users.noreply.github.com>
2019-08-23 16:34:57 -0500

    Add files via upload

--- element.js
+++ element.js
@@ -1,10 +1,10 @@
 document.body.onload = addElement;
 function addElement () {
   let newDiv = document.createElement('div');
-  let newContent = document.createTextNode('top');
+  let newContent = document.createTextNode('Top text');
   let currentDiv = document.getElementById('div1');
   newDiv.appendChild(newContent);
-  newDiv.classList.add('')
   document.body.insertBefore(newDiv, currentDiv);
-  
+  let x = document.getElementById('div1');
+  alert(x);
 }
